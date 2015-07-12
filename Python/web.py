0580f1ad61346ecceb0afa6279b2ccc3c083ebe0
Joshua Willigers <jwilligers@gmail.com>
2015-07-12 15:41:43 +1000

    Added template

--- Python/web.py
+++ Python/web.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World</h1>'
+	user = {'nickname': 'Josh'}
+	return render_template('index.html',title='Home',user=user)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
