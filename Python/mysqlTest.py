8a2f4ca81be75b4e85142cd153997e16dde682cb
Joshua Willigers <jwilligers@gmail.com>
2015-07-29 10:22:38 +0000

    Database adding data works

--- Python/mysqlTest.py
+++ Python/mysqlTest.py
@@ -1,6 +1,17 @@
+#!/usr/bin/python
 import MySQLdb
+import time
 
 
 db = MySQLdb.connect("localhost", "root", "elect1", "SensorData")
 
 curs=db.cursor()
+
+id = 4
+timestamp = int(time.time())
+temp = 23.54
+curs.execute ("INSERT INTO data values("+str(id)+ ","+str(timestamp)+","+ str(temp)+")")
+
+db.commit()
+print "Data committed"
+
