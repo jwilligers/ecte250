a7d16ec6e15ee97d53c35adedea9799f1cabbcdb
Joshua Willigers <jwilligers@gmail.com>
2015-07-30 18:26:52 +1000

    Added mysql connection to flask

--- Python/web.py
+++ Python/web.py
@@ -1,6 +1,22 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask.ext.mysql import MySQL
+
 app = Flask(__name__)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'elect1'
+app.config['MYSQL_DATABASE_DB'] = 'SensorData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
 
 @app.route('/')
 def index():
