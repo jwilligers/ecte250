5bb1f0654492e777faa2d1a764140615214385d8
Josh Willigers <jwilligers@gmail.com>
2015-07-31 18:57:12 +1000

    Added mysql python programs for clearing and creating test data

---
+++ Python/clearDB.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import MySQLdb
+import time
+
+
+db = MySQLdb.connect("localhost", "root", "elect1", "SensorData")
+
+curs=db.cursor()
+
+curs.execute ("DELETE * FROM data")
+
+db.commit()
+print "Database cleared"
