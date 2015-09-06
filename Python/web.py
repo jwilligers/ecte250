9a1329ec4f4aa7a19c2f56aa417223e2d25aa782
Josh Willigers <jwilligers@gmail.com>
2015-09-06 21:04:04 +1000

    Added graphs for all sensors, style changes, added more pygal settings

--- Python/web.py
+++ Python/web.py
@@ -24,33 +24,35 @@ config.show_legend = False
 config.human_readable = True
 config.show_x_labels = True
 config.height = 400
-config.width = 500
+config.width = 550
 config.interpolate = 'cubic'
 config.range = (0, 40)
 config.human_readable = True
-config.truncate_label =  -1
+config.show_dots = False
+config.truncate_label = -1
 
 
-gasID = {'CO':0, 'temp':1, 'humidity':2, 'LPG':3}
-gasArray = ["CO", "Carbon Monoxide", 0 , 50],["temp", "Temperature", 0 , 50],["humidity", "Humidity", 0 , 100]
+gasID = {'CO': 0, 'temp': 1, 'humidity': 2, 'LPG': 3}
+gasArray = ["CO", "Carbon Monoxide", 0, 50],["temp", "Temperature", 0, 50],["humidity", "Humidity", 0, 100]
+
 
 @app.route('/')
 @app.route('/Home')
 def index():
     user = {'nickname': 'Josh'}
-    return render_template('index.html', title='Home', user=user, selected ="Home")
+    return render_template('index.html', title='Home', user=user, selected="Home")
 
 
 @app.route('/Settings')
 def settings():
     user = {'nickname': 'usename'}
-    return render_template('index.html',title='Settings',user=user, selected = "Settings")
+    return render_template('index.html', title='Settings', user=user, selected="Settings")
 
 
 @app.route('/gas/<gas>')
 def showGasInfo(gas):
     user = {'nickname': 'Hudson'}
-    return render_template('gasInfo.html',title=gas, gas = gas, user=user)
+    return render_template('gasInfo.html', title=gas, gas=gas, user=user)
 
 
 @app.route('/barchart/<gas>/<time>')
@@ -61,21 +63,29 @@ def forecast(gas, time):
     id = []
     temperature = []
     humidity = []
+    pir_detection = []
+    var_resistor = []
 
+    line_chart = pygal.Line(config)
+#    line_chart.title = gasArray[gasID[gas]][1]
+    line_chart.x_labels = id
     for rows in results:
         id.append(int(results[i][0]))
+        pir_detection.append((int(results[i][1])))
         temperature.append(int(results[i][2]))
         humidity.append(int(results[i][3]))
+        var_resistor.append(int(results[i][4]))
         i += 1
+    if gas == "temp":
+        line_chart.add('Temperature', temperature)
+    if gas == "humidity":
+        line_chart.add('Humidity', humidity)
+    if gas == "pir":
+        line_chart.add('PIR Detection', pir_detection)
+    if gas == "var":
+        line_chart.add('Variable Resistor', var_resistor)
 
-        """ render svg graph """
-        line_chart = pygal.Line(config)
-        line_chart.title = gasArray[gasID[gas]][1]
-        if (gas == "temp"):
-            line_chart.add('Temperature', temperature)
-        if (gas == "humidity"):
-            line_chart.add('Humidity', humidity)
-        line_chart.x_labels = id
+
     return Response(response=line_chart.render(), content_type='image/svg+xml')
 
 if __name__ == '__main__':
