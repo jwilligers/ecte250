d3a0cd3a11b98c3c2bb67edbd478a8af06e3b177
Joshua Willigers <jwilligers@gmail.com>
2015-10-26 17:56:09 +1100

    Arduino/Pi serial communication test.

--- Python/arduino2.py
+++ Python/arduino2.py
@@ -9,36 +9,39 @@ curs = db.cursor()
 
 createDatabase = True
 
-source = "CSV"  # CSV or Serial
+source = "Serial"  # CSV or Serial
 i = 1
 
 # MQ2,MQ4,MQ6,MQ9,Humidity,Temp
 if createDatabase:
-    curs.execute("DROP TABLE IF EXISTS sensorData")
-    curs.execute("""	CREATE TABLE sensorData (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    flammablegas FLOAT NULL DEFAULT NULL,
-    methane FLOAT NULL DEFAULT NULL,
-    LPG FLOAT NULL DEFAULT NULL,
-    carbonmonoxide FLOAT NULL DEFAULT NULL,
-    temperature FLOAT NULL DEFAULT NULL,
-    humidity FLOAT NULL DEFAULT NULL
-    )
-    """)
+	print("Dropping Table")
+	curs.execute("DROP TABLE IF EXISTS sensorData")
+	print("Creating table")
+	curs.execute("""	CREATE TABLE sensorData (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	flammablegas FLOAT NULL DEFAULT NULL,
+	methane FLOAT NULL DEFAULT NULL,
+	LPG FLOAT NULL DEFAULT NULL,
+	carbonmonoxide FLOAT NULL DEFAULT NULL,
+	temperature FLOAT NULL DEFAULT NULL,
+	humidity FLOAT NULL DEFAULT NULL
+	)
+	""")
+	db.commit()
 
 if source == "CSV":
-    filename = "gasData.txt"
-    with open(filename) as f:
-        for line in f:
-            values = line.split(",")
-            flammablegas = values[0]
-            methane = values[1]
-            carbonmonoxide = values[2]
-            lpg = values[3]
-            humidity = values[4]
-            temperature = values[5]
+	filename = "gasData.txt"
+	with open(filename) as f:
+		for line in f:
+			values = line.split(",")
+			flammablegas = values[0]
+			methane = values[1]
+			carbonmonoxide = values[2]
+			lpg = values[3]
+			humidity = values[4]
+			temperature = values[5]
 
-            curs.execute("INSERT INTO sensorData VALUES(" +
+			curs.execute("INSERT INTO sensorData VALUES(" +
                          str(i) + "," +
                          str(flammablegas) + "," +
                          str(methane) + "," +
@@ -46,14 +49,40 @@ if source == "CSV":
                          str(lpg) + "," +
                          str(humidity) + "," +
                          str(temperature) + ")")
-            db.commit()
-            i += 1
+			db.commit()
+			i += 1
 
-    print ("Data committed")
-    print(str(i))
+	print ("Data committed")
+	print(str(i))
 
 if source == "Serial":
-    ser = serial.Serial()
-    print(ser.portstr)
-    while True:
-        values = ser.readline()
+	ser = serial.Serial('/dev/ttyACM0', 9600)
+	print(ser.portstr)
+	i = 0
+	while True:
+		i += 1
+		values = ser.readline()
+		values = ser.readline().split(",")
+		print(values)
+		if (len(values) == 6):
+			flammablegas = values[0]
+			methane = values[1]
+			carbonmonoxide = values[2]
+			lpg = values[3]
+			humidity = values[4]
+			temperature = values[5]
+
+			curs.execute("INSERT INTO sensorData VALUES(" +
+			str(i) + "," +
+			str(flammablegas) + "," +
+			str(methane) + "," +
+			str(carbonmonoxide) + "," +
+			str(lpg) + "," +
+			str(humidity) + "," +
+			str(temperature) + ")")
+			db.commit()
+			i += 1
+			print ("Data committed")
+			print(str(i))
+		else:
+			print ("Invalid Data")
